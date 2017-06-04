67dd448d608d7f0ea6579dd285fb8b492b271df5
Ron Bruun <ronald.bruun@libertymutual.com>
2017-06-04 18:19:47 -0400

    updates made for jquery exercises

--- medium/assets/javascript/medium.js
+++ medium/assets/javascript/medium.js
@@ -42,7 +42,57 @@
   //code in here wont run until page loads
   $(function(){
 
+    //make sure the page is loaded
 
+        let reset = $("#reset");
+        let resetCnt = $("#num-resets");
+        let teamOneShoot = $("#teamone-shoot");
+        let teamOneShots = $("#teamone-numshots");
+        let teamOneHits = $("#teamone-numhits");
+        let teamTwoShoot = $("#teamtwo-shoot");
+        let teamTwoShots = $("#teamtwo-numshots");
+        let teamTwoHits = $("#teamtwo-numhits");
+        let fanFare = new Audio("assets/audio/fanfare_x.wav");
+        let missed = new Audio("assets/audio/boing_x.wav");
+        let resetSnd = new Audio("assets/audio/cash_register_x.wav")
+        let pageHTML = $("html");
+
+        // set all of the shot counters back to 0 when the reset button is hit
+        reset.click(function () {
+            teamOneShots.html(0);
+            teamTwoShots.html(0);
+            teamOneHits.html(0);
+            teamTwoHits.html(0);
+            resetCnt.html(parseInt(resetCnt.html()) + 1);
+            resetSnd.play();
+            pageHTML.css({"background-color": ""});
+        });
+
+        teamOneShoot.click(function () {
+            if (checkShot(teamOneShots, teamOneHits)) {
+                pageHTML.css({"background-color": "#c5050c"});
+            };
+            //            
+        });
+
+        teamTwoShoot.click(function () {
+            if (checkShot(teamTwoShots, teamTwoHits)) {
+                pageHTML.css({"background-color": "#ffcc33"});
+            }
+        });
+
+        // common method that handles the shot button clicks
+        function checkShot(shots, hits) {
+            shots.html(parseInt(shots.html()) + 1);
+            if (Math.random() > .6) {
+                hits.html(parseInt(hits.html()) + 1);
+                fanFare.play();
+                return true;
+            } else {
+                missed.play();
+                return false;
+            }
+        }
 
   })
 
